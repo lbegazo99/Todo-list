e568c4e9f2d29f0cce960d2cb6f4474c047bcf5a
Luis Begazo <luisbegazo99@gmail.com>
2025-02-16 20:03:31 -0500

    styled the add todo module so that users will have to enter a valid date and are now able to click a button to set a priority intead of writing a color

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Project from "./todo_list.js";
 import Todo_items from "./todos.js";
 import "./styles.css";
-
+import { format,isValid } from "date-fns";
 const sidebar = document.getElementById("sidebar");
 const content = document.getElementById("content");
 
@@ -16,6 +16,8 @@ let project_div = document.createElement("div");
 let list_of_projects = document.createElement("ul")
 let todo_modal = document.createElement("dialog");
 todo_modal.classList.add("todo_modal")
+
+
 
 
 let home = new Project([],"home",0);
@@ -60,23 +62,45 @@ function create_todo_form(){
     let title_div = document.createElement("div");
     title_div.id = "title_div";
     title_div.contentEditable = true;
-    title_div.textContent = "enter a title";
+    title_div.textContent = "Title";
 
     let description_div = document.createElement("div");
     description_div.id = "description_div";
     description_div.contentEditable = true;
-    description_div.textContent = "Enter a todo";
-    let description_div_caption = document.createElement("p");
-    description_div_caption.textContent = "description";
-    description_div.appendChild(description_div_caption);
+    description_div.textContent = "Description";
 
     let date_div = document.createElement("div");
     date_div.contentEditable = true;
-    date_div.textContent = "When would you like to do this"
+    date_div.textContent = "mm/dd/yyyy"
 
+    let color = ""
     let priority_div = document.createElement("div");
-    priority_div.contentEditable = true;
-    priority_div.textContent = "Enter the priority level by entering red, orange, and blue"   
+    
+    let red_indiciator =  create_indicator("red");
+    let yellow_indicator = create_indicator("yellow");
+    let green_indicator = create_indicator("green");
+
+    let red_radio = create_radio()
+    red_radio.addEventListener("click",function (){
+        color = "red"
+    })
+    let yellow_radio = create_radio();
+    yellow_radio.addEventListener("click",function (){
+        color = "yellow"
+    })
+    let green_radio = create_radio();
+    green_radio.addEventListener("click",function (){
+        color = "green"
+    })
+
+
+    priority_div.append(red_indiciator)
+    priority_div.appendChild(red_radio)
+    priority_div.appendChild(yellow_indicator)
+    priority_div.appendChild(yellow_radio)
+    priority_div.appendChild(green_indicator)
+    priority_div.appendChild(green_radio)
+    priority_div.style.display = "flex";
 
 
     let footer_div = document.createElement("div");
@@ -87,7 +111,7 @@ function create_todo_form(){
     cancel_button.textContent = "Cancel"
     let add_task_button = document.createElement("button");
     add_task_button.addEventListener("click",function(){
-        addTask(title_div.textContent,description_div.textContent,date_div.textContent,priority_div.textContent,curr_project_id);
+        addTask(title_div.textContent,description_div.textContent,date_div.textContent,color,curr_project_id,date_div);
     })
     add_task_button.textContent = "Add Task"
     footer_div.appendChild(cancel_button);
@@ -101,12 +125,17 @@ function create_todo_form(){
     content.appendChild(create_task_block);
 }
 
-function addTask(title,description,date,priority,project_id){
+function addTask(title,description,date,priority,project_id,date_div){
     let this_project = projects[project_id];
-    create_task_block.close()
-    this_project.add_to_project(new Todo_items(title,description,date,priority))
-    list_all_todos(project_id);
-   
+    let parts = date.split("/");
+    try {
+        format(new Date(parts[0],parts[1],parts[2]),"MM/dd/yyyy");
+        create_task_block.close();
+        this_project.add_to_project(new Todo_items(title,description,date,priority))
+        list_all_todos(project_id);
+    } catch (error) {
+        date_div.textContent = "please enter the date in the correct format like MM/dd/yyyy";
+    }
 }
 
 // list all of the projects 
@@ -171,7 +200,7 @@ function create_project_form(){
     })
     let submit_button = document.createElement("button")
     submit_button.addEventListener("click",function () {
-        create_project_block.style.display = "none"
+        create_project_block.close()
         projects.push(new Project([],project_name.textContent,projects.length))
         list_all_projects();
     })
@@ -222,4 +251,19 @@ function fill_todo_modal(this_project,j){
     document.querySelector(".todo_modal > p").textContent = this_project.get_todo(j).description;
     document.querySelector(".todo_modal > .dueDate").textContent = this_project.get_todo(j).dueDate;
 
+}
+function create_radio(){
+    let radio = document.createElement("input");
+    radio.type = "radio";
+    radio.id = "radio";
+    radio.name = "radio"
+    return radio
+
+}
+
+function create_indicator(color){
+    let indiciator = document.createElement("div");
+    indiciator.style.backgroundColor = color
+    indiciator.style.width = '15px'
+    return indiciator;
 }
